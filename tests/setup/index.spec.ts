121b58719d12489efcd9445ee82fcc5b60e6c416
porkytheblack <porkytheblack@gmail.com>
2025-03-22 20:54:49 +0300

    Remove Reserve tests and related logic from setup tests

--- tests/setup/index.spec.ts
+++ tests/setup/index.spec.ts
@@ -18,45 +18,5 @@ describe("Setup Lender and Issuer", async()=>{
     const reserveContract = ContractId.fromString(RESERVE_CONTRACT_ID)
     const lenderContract = ContractId.fromString(LENDER_CONTRACT_ID)
 
-    test("Issuer, Lender => Reserve", async ()=>{
 
-        const tx = await new ContractExecuteTransaction()
-        .setContractId(reserveContract)
-        .setGas(1000000)
-        .setFunction("addControllers",
-            new ContractFunctionParameters()
-            .addAddress(issuerContract.toSolidityAddress())
-            .addAddress(lenderContract.toSolidityAddress())        
-        )
-        .freezeWith(client)
-        .sign(admin.PRIVATE_KEY)
-
-        const submittedTx = await tx.execute(client)
-        const receipt = await submittedTx.getReceipt(client)
-
-        console.log(`TX ID: ${submittedTx.transactionId}`)
-        console.log(`Status: ${receipt.status}`)
-
-        
-
-    })
-
-
-    test("Lender => Issuer", async ()=>{
-        const tx = await new ContractExecuteTransaction()
-        .setContractId(issuerContract)
-        .setGas(1000000)
-        .setFunction("setLender",
-            new ContractFunctionParameters()
-            .addAddress(lenderContract.toSolidityAddress())
-        )
-        .freezeWith(client)
-        .sign(admin.PRIVATE_KEY)
-
-        const submittedTx = await tx.execute(client)
-        const receipt = await submittedTx.getReceipt(client)
-
-        console.log(`TX ID: ${submittedTx.transactionId}`)
-        console.log(`Status: ${receipt.status}`)
-    })
 })
