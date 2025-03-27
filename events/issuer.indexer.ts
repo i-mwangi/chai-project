5c2d529efca5d48c7b6410214e8dabcfade0a668
porkytheblack <porkytheblack@gmail.com>
2025-03-27 06:08:18 +0300

    fix: write to db

--- events/issuer.indexer.ts
+++ events/issuer.indexer.ts
@@ -6,73 +6,73 @@ import { indexFirestore } from "./utils";
 
 indexFirestore({
     contract: 'issuer',
+    ignoreLastCommit: true,
     processor: async (key, data) => {
         console.log("Processing key", key)
         // console.log("Processing data", data)
-        return;
-        // switch(data.type){
-        //     case "AssetMinted":{
-        //         console.log("Asset Minted", data)
+        switch (data.type) {
+            case "AssetMinted": {
+                console.log("Asset Minted", data)
 
-        //         break
-        //     }
-        //     case "AssetBurned":{
-        //         console.log("Asset Burned", data)
-        //         break
-        //     }
-        //     case "KYCGranted": {
-        //         console.log("KYC Granted", data)
-        //         const existing = await db.query.kyc.findFirst({
-        //             where: (fields, ops)=> ops.eq(fields.account, data.account)
-        //         })
-        //         if(existing){
-        //             console.log("KYC already granted")
-        //             break
-        //         }
-        //         await db.insert(kyc).values({
-        //             account: data.account,
-        //             token: data.token,
-        //         })
-        //         break
-        //     }
-        //     case "AssetPurchased": {
-        //         console.log("Asset Purchased", data)
-        //         await db.insert(transactions).values({
-        //             hash: data.hash,
-        //             account: data.buyer,
-        //             token: data.asset,
-        //             amount: data.amount,
-        //             type: "buy",
-        //             timestamp: data.timestamp
-        //         })
-        //         break
-        //     }
-        //     case "AssetSold": {
-        //         console.log("Asset Sold", data)
-        //         await db.insert(transactions).values({
-        //             hash: data.hash,
-        //             account: data.seller,
-        //             token: data.asset,
-        //             amount: data.amount,
-        //             type: "sell",
-        //             timestamp: data.timestamp
-        //         })
-        //         break
-        //     }
-        //     case "AssetCreated": {
-        //         console.log("Asset Created", data)
-        //         await db.insert(assets).values({
-        //             token: data.token,
-        //             name: data.name,
-        //             symbol: data.symbol,
-        //             timestamp: data.timestamp
-        //         })
-        //         break
-        //     }
-        //     default:{
-        //         console.log("Unknown event type", data.type)
-        //     }
-        // }
+                break
+            }
+            case "AssetBurned": {
+                console.log("Asset Burned", data)
+                break
+            }
+            case "KYCGranted": {
+                console.log("KYC Granted", data)
+                const existing = await db.query.kyc.findFirst({
+                    where: (fields, ops) => ops.eq(fields.account, data.account)
+                })
+                if (existing) {
+                    console.log("KYC already granted")
+                    break
+                }
+                await db.insert(kyc).values({
+                    account: data.account,
+                    token: data.token,
+                })
+                break
+            }
+            case "AssetPurchased": {
+                console.log("Asset Purchased", data)
+                await db.insert(transactions).values({
+                    hash: data.hash,
+                    account: data.buyer,
+                    token: data.asset,
+                    amount: data.amount,
+                    type: "buy",
+                    timestamp: data.timestamp
+                })
+                break
+            }
+            case "AssetSold": {
+                console.log("Asset Sold", data)
+                await db.insert(transactions).values({
+                    hash: data.hash,
+                    account: data.seller,
+                    token: data.asset,
+                    amount: data.amount,
+                    type: "sell",
+                    timestamp: data.timestamp
+                })
+                break
+            }
+            case "AssetCreated": {
+                console.log("Asset Created", data)
+                await db.insert(assets).values({
+                    token: data.token,
+                    name: data.name,
+                    symbol: data.symbol,
+                    timestamp: data.timestamp
+                })
+                break
+            }
+            default: {
+                console.log("Unknown event type", data.type)
+            }
+        }
     },
     store: issuerFireStore
 })
--- events/lender.indexer.ts
+++ events/lender.indexer.ts
@@ -7,103 +7,103 @@ import { indexFirestore } from "./utils";
 
 indexFirestore({
     contract: 'lender',
+    ignoreLastCommit: true,
     processor: async (key, data) => {
         console.log("Processing key", key)
         console.log("Processing data", data)
-        return;
-        // switch(data.type){
-        //     case "AssetLendingReserveCreated": {
+        switch (data.type) {
+            case "AssetLendingReserveCreated": {
                 
-        //         await db.insert(lendingReserves).values({
-        //             token: data.token,
-        //             asset: data.asset,
-        //             name: data.name,
-        //             symbol: data.name, // TODO: Fix to use symbol
-        //             timestamp: data.timestamp
-        //         })
-        //         break;
-        //     }
-        //     case "LoanRecorded": {
-        //         await db.insert(loans).values({
-        //             account: data.borrower,
-        //             collateralAmount: data.collateralAmountAsset,
-        //             collateralAsset: data.token,
-        //             id: generateId("loan"),
-        //             liquidationPrice: data.liquidationUSDCPrice,
-        //             loanAmountUSDC: data.loanAmountUSDC,
-        //             repaymentAmount: data.repayAmount,
-        //             timestamp: data.timestamp
-        //         })
-        //         break;
-        //     }
-        //     case "LoanLiquidated": {
-        //         const borrower = data.borrower
-        //         const existingLoan = await db.query.loans.findFirst({
-        //             where(fields, operators) {
-        //                 return operators.and(
-        //                     operators.eq(fields.account, borrower),
-        //                     operators.eq(fields.collateralAsset, data.token)
-        //                 )
-        //             },
-        //         })
+                await db.insert(lendingReserves).values({
+                    token: data.token,
+                    asset: data.asset,
+                    name: data.name,
+                    symbol: data.name, // TODO: Fix to use symbol
+                    timestamp: data.timestamp
+                })
+                break;
+            }
+            case "LoanRecorded": {
+                await db.insert(loans).values({
+                    account: data.borrower,
+                    collateralAmount: data.collateralAmountAsset,
+                    collateralAsset: data.token,
+                    id: generateId("loan"),
+                    liquidationPrice: data.liquidationUSDCPrice,
+                    loanAmountUSDC: data.loanAmountUSDC,
+                    repaymentAmount: data.repayAmount,
+                    timestamp: data.timestamp
+                })
+                break;
+            }
+            case "LoanLiquidated": {
+                const borrower = data.borrower
+                const existingLoan = await db.query.loans.findFirst({
+                    where(fields, operators) {
+                        return operators.and(
+                            operators.eq(fields.account, borrower),
+                            operators.eq(fields.collateralAsset, data.token)
+                        )
+                    },
+                })
 
-        //         if(existingLoan){
-        //             await db.insert(liquidations).values({
-        //                 id: generateId("liquidation"),
-        //                 account: borrower,
-        //                 loanId: existingLoan.id,
-        //                 timestamp: data.timestamp
-        //             })
-        //         }
-        //         break;
-        //     }
-        //     case "LoanRepaid": {
-        //         const borrower = data.borrower
-        //         const existingLoan = await db.query.loans.findFirst({
-        //             where(fields, operators) {
-        //                 return operators.and(
-        //                     operators.eq(fields.account, borrower),
-        //                     operators.eq(fields.collateralAsset, data.token)
-        //                 )
-        //             },
-        //         })
+                if (existingLoan) {
+                    await db.insert(liquidations).values({
+                        id: generateId("liquidation"),
+                        account: borrower,
+                        loanId: existingLoan.id,
+                        timestamp: data.timestamp
+                    })
+                }
+                break;
+            }
+            case "LoanRepaid": {
+                const borrower = data.borrower
+                const existingLoan = await db.query.loans.findFirst({
+                    where(fields, operators) {
+                        return operators.and(
+                            operators.eq(fields.account, borrower),
+                            operators.eq(fields.collateralAsset, data.token)
+                        )
+                    },
+                })
 
-        //         if(existingLoan){
-        //             await db.insert(loanRepayment).values({
-        //                 id: generateId("repayment"),
-        //                 account: borrower,
-        //                 loanId: existingLoan.id,
-        //                 token: data.token,
-        //                 timestamp: data.timestamp
-        //             })
-        //         }
-        //         break;
-        //     }
-        //     case "LiquidityProvided": {
-        //         await db.insert(providedLiquidity).values({
-        //             id: generateId("liquidity"),
-        //             account: data.user,
-        //             asset: data.asset,
-        //             amount: data.amount,
-        //             timestamp: data.timestamp
-        //         })
-        //         break;
-        //     }
-        //     case "LiquidityWithdrawn": {
-        //         await db.insert(withdrawnLiquidity).values({
-        //             id: generateId("withdrawal"),
-        //             account: data.user,
-        //             asset: data.asset,
-        //             amount: data.amount,
-        //             timestamp: data.timestamp
-        //         })
-        //         break;
-        //     }
-        //     default: {
-        //         console.log("Unknown event type", data)
-        //         break
-        //     }
-        // }
+                if (existingLoan) {
+                    await db.insert(loanRepayment).values({
+                        id: generateId("repayment"),
+                        account: borrower,
+                        loanId: existingLoan.id,
+                        token: data.token,
+                        timestamp: data.timestamp
+                    })
+                }
+                break;
+            }
+            case "LiquidityProvided": {
+                await db.insert(providedLiquidity).values({
+                    id: generateId("liquidity"),
+                    account: data.user,
+                    asset: data.asset,
+                    amount: data.amount,
+                    timestamp: data.timestamp
+                })
+                break;
+            }
+            case "LiquidityWithdrawn": {
+                await db.insert(withdrawnLiquidity).values({
+                    id: generateId("withdrawal"),
+                    account: data.user,
+                    asset: data.asset,
+                    amount: data.amount,
+                    timestamp: data.timestamp
+                })
+                break;
+            }
+            default: {
+                console.log("Unknown event type", data)
+                break
+            }
+        }
     },
     store: lenderFireStore
 })
